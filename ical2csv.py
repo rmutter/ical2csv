bf3af5f738bcd851d4f668c36ad384e3655b8bce
Rudy Mutter <rudy@yeti.co>
2024-08-01 15:20:44 -0700

    updating for parsing unique event attendees

--- ical2csv.py
+++ ical2csv.py
@@ -14,16 +14,16 @@ from icalendar import Calendar
 class CalendarEvent:
     """Calendar event class"""
     summary = ''
-    uid = ''
     description = ''
     location = ''
     start = ''
     end = ''
-    url = ''
+    attendees = ''
     CALENDAR_TIMEZONE = None
 
 
 def open_cal():
+    seen_emails = {}
     events = []
     if os.path.isfile(filename):
         if file_extension == 'ics':
@@ -31,21 +31,67 @@ def open_cal():
             f = open(sys.argv[1], 'rb')
             gcal = Calendar.from_ical(f.read())
             gcal.CALENDAR_TIMEZONE = pytz.timezone(gcal.get("X-WR-TIMEZONE").encode('utf8').decode())
+            count = 0
             for component in gcal.walk():
+                # if count == 1:
+                #     break
+
                 event = CalendarEvent()
-                if component.get('TRANSP') == 'TRANSPARENT': continue  # skip event that have not been accepted
+
+                # if component.get('TRANSP') == 'TRANSPARENT': continue  # skip event that have not been accepted
+
                 if component.get('SUMMARY') is None: continue  # skip blank items
                 event.summary = component.get('SUMMARY')
-                event.uid = component.get('UID')
-                if component.get('DESCRIPTION') is None: continue  # skip blank items
-                event.description = component.get('DESCRIPTION')
+                
+                if component.get('DESCRIPTION') is None:
+                    event.description = ''
+                else:
+                    event.description = component.get('DESCRIPTION')
+
                 event.location = component.get('LOCATION')
+
                 if hasattr(component.get('dtstart'), 'dt'):
                     event.start = component.get('dtstart').dt
+
                 if hasattr(component.get('dtend'), 'dt'):
                     event.end = component.get('dtend').dt
-                event.url = component.get('URL')
-                events.append(event)
+
+                # print(component)
+                parsed_attendees = ''
+                if component.get('ATTENDEE') is not None:
+                    attendees = component.get("ATTENDEE")
+
+                    # Skip if this is a string, it will just be my personal email
+                    if type(attendees) is str:
+                        continue
+
+                    for attendee in attendees:
+                        # Skip if this is a string, it will just be my personal email
+                        if type(attendee) is str:
+                            continue
+
+                        email = str(attendee)
+                        email = email.removeprefix("mailto:")
+
+                        name = 'N/A'
+                        if 'CN' in attendee.params and attendee.params['CN'] != email:
+                            name = attendee.params['CN']
+
+                        # Skip Yeti emails
+                        if '@yeti' not in email:
+                            if email not in seen_emails:
+                                seen_emails[email] = 1
+
+                                if len(parsed_attendees) > 0:
+                                    parsed_attendees += '\n'
+                                parsed_attendees += name + ": " + email
+
+                if len(parsed_attendees) == 0:
+                    continue
+
+                event.attendees = parsed_attendees
+
+                events.append(event)                
             f.close()
             return events, gcal
         else:
@@ -65,9 +111,7 @@ def csv_write(icsfile):
             wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
             wr.writerow(headers)
             for event in sortedevents:
-                values = (event.summary.encode('utf8').decode(), event.uid,
-                          event.description.encode('utf8').decode(),
-                          event.location, event.start, event.end, event.url)
+                values = (event.summary.encode('utf8').decode(), event.description.encode('utf8').decode(), event.location, event.attendees, event.start, event.end)
                 wr.writerow(values)
             print("Wrote to ", csvfile, "\n")
     except IOError:
@@ -76,14 +120,12 @@ def csv_write(icsfile):
 
 
 def debug_event(class_name):
-    print("Contents of ", class_name.name, ":")
-    print(class_name.summary)
-    print(class_name.uid)
+    print("Contents of ", class_name.summary, ":")
     print(class_name.description)
+    print(class_name.attendees)
     print(class_name.location)
     print(class_name.start)
-    print(class_name.end)
-    print(class_name.url, "\n")
+    print(class_name.end, "\n")
 
 
 def format_all_day_events(event_list, default_timezone=pytz.timezone("UTC"),
@@ -101,6 +143,7 @@ def format_all_day_events(event_list, default_timezone=pytz.timezone("UTC"),
             # print(timezone)
             # change event's start and end time to some arbitrary time
             # I prefer 9 - 5 events
+            # debug_event(e)
             e.start = datetime.datetime.combine(e.start, default_start_time)
             e.end = datetime.datetime.combine(e.end, default_end_time)
 
@@ -121,7 +164,7 @@ if __name__ == "__main__":
     filename = sys.argv[1]
     # TODO: use regex to get file extension (chars after last period), in case it's not exactly 3 chars.
     file_extension = str(sys.argv[1])[-3:]
-    headers = ('Summary', 'UID', 'Description', 'Location', 'Start Time', 'End Time', 'URL')
+    headers = ('Summary', 'Description', 'Location', 'Attendees', 'Start Time', 'End Time')
     events, calendar = open_cal()
     formatted_events = format_all_day_events(events, default_timezone=calendar.CALENDAR_TIMEZONE)
     sortedevents = sorted(formatted_events, key=lambda obj: obj.start)
